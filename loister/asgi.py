8b8d1f8fe0f2f41d3f20a406eb3ddfb878e728e6
Sina <sina.khosravi.ac@gmail.com>
2023-11-02 12:37:22 +0330

    fix project root directory name based on conventions

--- loister/asgi.py
+++ loister/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MrRobot project.
+ASGI config for loister project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MrRobot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loister.settings')
 
 application = get_asgi_application()
