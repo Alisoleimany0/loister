fad9a85804c65f2b0ba9af39fa59aac2089f74ed
ali <you@example.com>
2023-12-24 18:21:54 +0330

    add postal_code

--- shop/models.py
+++ shop/models.py
@@ -15,9 +15,10 @@ class Customer(models.Model):
     phone = models.CharField(max_length=20)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=50)
+    postal_code = models.IntegerField(null=False, default=0)
 
     def __str__(self):
-        return f'{self.firstname} {self.last_name}'
+        return f'{self.first_name} {self.last_name}'
 
 
 class Product(models.Model):
@@ -25,7 +26,7 @@ class Product(models.Model):
     description = models.CharField(max_length=500, default='', blank=True, null=True)
     price = models.DecimalField(default=0, decimal_places=0, max_digits=12)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to='upload/product/')
+    picture = models.ImageField(null=True, upload_to='upload/product/')
     star = models.CharField(max_length=5, default='0', validators=[MaxLengthValidator(5), MinValueValidator('0')])
     # star = models.IntegerField(default=0 , validators=[MaxLengthValidator(5), MinValueValidator(0)])
     is_sale = models.BooleanField(default=False)
