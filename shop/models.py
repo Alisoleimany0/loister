f13341ccb6eed4206f411b139bb35fbabb98ba26
sina khosravi <sina.khosravi.ac@gmail.com>
2024-01-13 00:30:34 +0330

    comment out product offers creation validation

--- shop/models.py
+++ shop/models.py
@@ -84,8 +84,9 @@ class ProductOffers(models.Model):
 
     def save(self, *args, **kwargs):
         # make sure we're not updating
-        if not self.pk and HomepageCoverGroup.objects.exists():
-            raise ValidationError("You can only create one instance of ProductOffers")
+        print(HomepageCoverGroup.objects.exists())
+        # if not self.pk and HomepageCoverGroup.objects.exists():
+        #     raise ValidationError("You can only create one instance of ProductOffers")
         return super(ProductOffers, self).save(*args, **kwargs)
 
 
